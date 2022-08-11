debe838d477c9d5926080385afd4e8f54b18aee5
MantoDev97 <devmanto97@gmail.com>
2022-08-10 22:34:33 -0300

    Primeiro commit Tela de login

---
+++ lib/src/auth/compenets/custon_text_field.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class CustonTextField extends StatefulWidget {
+  final IconData icon;
+  final String label;
+  final bool isSecret;
+  
+
+   const CustonTextField(
+      {Key? key,
+      required this.icon,
+      required this.label,
+      this.isSecret = false})
+      : super(key: key);
+
+  @override
+  State<CustonTextField> createState() => _CustonTextFieldState();
+}
+
+class _CustonTextFieldState extends State<CustonTextField> {
+
+   bool isObscure = false;
+
+   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    isObscure = widget.isSecret;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15),
+      child: TextFormField(
+        obscureText: isObscure,
+        decoration: InputDecoration(
+          prefixIcon: Icon(widget.icon),
+          suffixIcon: widget.isSecret ? IconButton(
+            onPressed: () {
+              setState(() {
+                isObscure = !isObscure;
+              });
+            },
+            icon: Icon(isObscure ? Icons.visibility : Icons.visibility_off),
+          ): null,
+          labelText: widget.label,
+          isDense: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,182 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+
+import 'compenets/custon_text_field.dart';
+import 'config/custom_colors.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: CustomColors.CustomSwathColor,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          child: Column(
+            children: [
+              Expanded(
+                child:
+                    Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  //NOME DO APP
+                   Text.rich(
+                    TextSpan(
+                      style: const TextStyle(
+                        fontSize: 40,
+                      ),
+                      children: [
+                        const TextSpan(
+                          text: 'Fruit',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Manto',
+                          style: TextStyle(
+                            color: CustomColors.CustomContrastColors,
+                            //fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+      
+                  //CATEGORIAS
+                  SizedBox(
+                    height: 30,
+                    child: DefaultTextStyle(
+                      style: const TextStyle(
+                        fontSize: 25,
+                      ),
+                      child: AnimatedTextKit(
+                        pause: Duration.zero,
+                        repeatForever: true,
+                        animatedTexts: [
+                          FadeAnimatedText('Frutas'),
+                          FadeAnimatedText('Verduras'),
+                          FadeAnimatedText('Legumes'),
+                          FadeAnimatedText('Carnes'),
+                          FadeAnimatedText('Cereais'),
+                          FadeAnimatedText('Laticinios'),
+                        ],
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+              //Formulário
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 40,
+                ),
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(50))),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      //EMAIL
+                      const CustonTextField(
+                        icon: Icons.email,
+                        label: 'Email',
+                      ),
+      
+                      //SENHA
+                      const CustonTextField(
+                        icon: Icons.lock,
+                        label: 'Senha',
+                        isSecret: true,
+                      ),
+      
+                      //BOTÃO ENTRAR
+                      SizedBox(
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20))),
+                          onPressed: () {},
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ),
+      
+                      //ESQUECEU A SENHA?
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {},
+                          child:  Text(
+                            'Esqueceu a senha?',
+                            style: TextStyle(color: CustomColors.CustomContrastColors),
+                          ),
+                        ),
+                      ),
+      
+                      //DIVISOR E OU
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Divider(
+                                color: Colors.grey.withAlpha(90),
+                                thickness: 2,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 15),
+                              child: Text('ou'),
+                            ),
+                            Expanded(
+                              child: Divider(
+                                color: Colors.grey.withAlpha(90),
+                                thickness: 2,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+      
+                      //BOTÃO CRIAR CONTA
+                      SizedBox(
+                        height: 50,
+                        child: OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18),
+                              ),
+                              side: const BorderSide(
+                                width: 2,
+                                color: Colors.green,
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'Criar Conta',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.green,
+                              ),
+                            )),
+                      )
+                    ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
