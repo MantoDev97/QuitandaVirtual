0e729bcf70cd6cdca85036d4895d1b0e2deb097c
MantoDev97 <devmanto97@gmail.com>
2022-08-24 00:56:55 -0300

    Adicionando icone customizado

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'FruitManto',
       theme: ThemeData(
     
         primarySwatch: Colors.green,
